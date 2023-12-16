0c548e81853d8e21b2d9f1adcdfdf12aa87161bb
Matvey Ryabchikov <35634442+ronanru@users.noreply.github.com>
2023-12-16 20:04:52 +0300

    fix mobile nav

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -3,9 +3,9 @@ import { Navigation } from "./navigation";
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <main className="flex-1 mx-auto max-w-[648px] w-full p-6">
-        {children}
-      </main>
+      <div className="h-full overflow-y-auto flex-1">
+        <main className="mx-auto max-w-[648px] w-full p-6">{children}</main>
+      </div>
       <Navigation />
     </>
   );
